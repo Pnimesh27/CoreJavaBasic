669f3f6907e11592822df593e470d0fb5c71b093
Nimesh Prajapati <Nimesh Prajapati@DESKTOP-5OBRHKG>
2021-07-22 19:14:49 +0530

    commit

---
+++ CoreJavaBasic/src/corejava/day6/oddoreven.java
@@ -0,0 +1,20 @@
+package corejava.day6;
+
+import java.util.Scanner;
+
+public class oddoreven {
+public static void main(String[] args) {
+	Scanner rohit= new Scanner(System.in);
+	
+	System.out.println("enter a number ");
+	int num= rohit.nextInt();
+	if(num%2==0)
+		System.out.println("even num");
+	else
+		System.out.println("odd num");
+
+
+	
+	
+}
+}
