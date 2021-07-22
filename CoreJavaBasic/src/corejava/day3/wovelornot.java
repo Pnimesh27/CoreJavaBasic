11aaa0efcd50bd0fcd69815165269298b1abf391
Nimesh Prajapati <Nimesh Prajapati@DESKTOP-5OBRHKG>
2021-07-22 17:42:09 +0530

    commit

---
+++ CoreJavaBasic/src/corejava/day3/wovelornot.java
@@ -0,0 +1,15 @@
+package corejava.day3;
+
+public class wovelornot {
+public static void main(String[] args) {
+	
+	char ch='i';
+	
+	if(ch =='a'|| ch =='i'|| ch =='e'|| ch =='o'|| ch =='u')
+		System.out.println(ch  );
+	else
+		System.out.println(ch+ "is consunant");
+	
+		
+}
+}
