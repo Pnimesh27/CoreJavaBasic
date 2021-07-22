b1e06e035bbaaf65c1af592a1588df438f93a2a6
Nimesh Prajapati <Nimesh Prajapati@DESKTOP-5OBRHKG>
2021-07-22 18:25:18 +0530

    commit

---
+++ CoreJavaBasic/src/corejava/day4/lasrgestamong3num.java
@@ -0,0 +1,17 @@
+package corejava.day4;
+
+public class lasrgestamong3num {
+public static void main(String[] args) {
+	int n1= 10, n2=4, n3=21;
+
+	
+	if(n1>=n2 && n1 >=n3)
+		System.out.println(n1+"is largest");
+	else if(n2>=n1 && n2>=n3)
+		System.out.println(n2+"is largest");
+	else
+		System.out.println(n3+"is largest");
+		
+		
+}
+}
