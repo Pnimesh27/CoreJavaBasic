fc1644a825121953d44dd1b281ab2a9bf23b2dd0
Nimesh Prajapati <Nimesh Prajapati@DESKTOP-5OBRHKG>
2021-07-24 13:36:08 +0530

    commit

---
+++ CoreJavaBasic/src/corejava/day10/dowhileloop.java
@@ -0,0 +1,15 @@
+package corejava.day10;
+
+public class dowhileloop {
+public static void main(String[] args) {
+	int x=4;
+	do {
+		System.out.println("value of x is "+x);
+		x++;
+System.out.println("\n");	
+	} while(x<6);
+	
+	
+	}
+}
+
