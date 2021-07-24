5bc405feae9ea62d9ec53d4af76188d15dd35a1d
Nimesh Prajapati <Nimesh Prajapati@DESKTOP-5OBRHKG>
2021-07-24 13:50:48 +0530

    commit

---
+++ CoreJavaBasic/src/corejava/eg1/switchcase.java
@@ -0,0 +1,21 @@
+package corejava.eg1;
+
+public class switchcase {
+public static void main(String[] args) {
+	int tech =1;
+	
+	switch (tech ) {
+	case 1:{
+		System.out.println("java");
+		break;
+	}
+	case 2:{
+		System.out.println("es2");
+		break;
+	}
+	default :{
+		System.out.println("not listed ");
+	}
+	}
+}
+}
