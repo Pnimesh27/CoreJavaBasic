88c5aa5d41d68c32dc682f4b117e6ccd78ac2b9f
Nimesh Prajapati <Nimesh Prajapati@DESKTOP-5OBRHKG>
2021-07-24 19:59:45 +0530

    commit

---
+++ CoreJavaBasic/src/corejava/eg10/sumofthearraysvalue.java
@@ -0,0 +1,13 @@
+package corejava.eg10;
+
+public class sumofthearraysvalue {
+public static void main(String[] args) {
+	int[] array= {10,20,30,40,50,10};
+	int sum=0;
+	for (int num:array) {
+		sum=sum+num;
+	}
+System.out.println("sum of array elements is:"+sum);
+}
+
+}
